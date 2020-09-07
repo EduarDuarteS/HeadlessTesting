640908619c3785fc54c7b65a2533c4ff4c6f3f64
Eduard <eduard.duarte@hotmail.com>
2020-09-06 20:56:02 -0500

    playwright headless done init

--- playwright-test/index.js
+++ playwright-test/index.js
@@ -3,7 +3,18 @@ const { webkit } = require('playwright');
 //Principal flow
 //It creates a new user and then does a login
 (async () => {
-  const browser = await webkit.launch();
+
+  //Se inicia el conteo de tiempo de ejecución
+  console.time("timeExecute");
+
+  // Memoria Inicial
+  console.log("###  Memoria Inicial ###");
+  var os = require('os');
+  console.log(os.totalmem());
+  console.log(os.freemem())
+  console.log(os.totalmem() - os.freemem());
+
+  const browser = await webkit.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://angular-6-registration-login-example.stackblitz.io/register');
 
@@ -17,15 +28,36 @@ const { webkit } = require('playwright');
   await page.click('button')
 
   await page.waitForTimeout(2000);
-  await page.screenshot({path: 'example.png'});
+  await page.screenshot({ path: 'example.png' });
 
   await (await page.$('input[formcontrolname=\'username\']')).type('Jhonnyguzz');
   await (await page.$('input[formcontrolname=\'password\']')).type('mypassword');
   await page.click('button')
 
   await page.waitForTimeout(2000);
-  await page.screenshot({path: 'example.png'});
+  await page.screenshot({ path: 'example.png' });
 
+  // Memoria Final
+  console.log("###  Memoria Final  ###");
+  var os = require('os');
+  console.log(os.totalmem());
+  console.log(os.freemem())
+  console.log(os.totalmem() - os.freemem());
+
+  //instalar npm install os-utils
+  var os = require('os-utils');
+
+  os.cpuUsage(function (v) {
+    console.log('CPU Usage (%): ' + v);
+  });
+
+  //se cierra el explorador
   await browser.close();
+
+  const used4 = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(`The script uses approximately ${used4} MB`);
+
+  console.timeEnd("timeExecute");
+
 
 })();
